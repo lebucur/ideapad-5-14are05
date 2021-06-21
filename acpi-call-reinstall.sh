a8e21089b4bd4d346880c7e7f8aa0f6fcbfe6780
Loredan <loredan.bucur@siiromania.ro>
2021-06-21 14:28:59 +0300

    for when updating the kernel

--- acpi-call-reinstall.sh
+++ acpi-call-reinstall.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #sudo apt reinstall acpi-call-lts #LTS kernel
-sudo apt reinstall acpi-call-dkms #non-LTS
+#sudo apt reinstall acpi-call-dkms #non-LTS
 
 sudo insmod /lib/modules/$(uname -r)/updates/dkms/acpi_call.ko
 
